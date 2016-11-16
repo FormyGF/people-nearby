2eba06db809f65c0353f5c0b806c71130a9515d0
Tianxiao Yang <yangtianxiao123@gmail.com>
2016-11-16 10:53:11 -0500

    able to send message to user with identity now

--- server.js
+++ server.js
@@ -1,34 +1,80 @@
 const app = require('express')();
-const server = require('http').createServer(app);
+const server = require('http').createServer(app, {pingTimeout: 60000, pingInterval: 25000});
 const io = require('socket.io')(server);
 
-let users = [];
-let clients = {};
+let users = {};
+let userSockets = {};
+let disConnectedUsers = {};
+
+app.use("/", (req,res) => {
+
+	res.json({message: "received"});
+
+})
 
 io.on('connection', function(client) {
-	// client.emit('fancyEvent', {some: "Hello world"});
 
 	console.log(`connected with ${client.id}`);
-	clients["/#" + client.id] = client;
+	userSockets["/#" + client.id] = client;
 
-	// users.add(client.id);
+	io.clients((err, clients) => {
 
-	client.on('disconnect', (message) => {
-		console.log(client);
-		// users.remove(client.id);
-		console.log(`disconnected: ${client}`);
+		console.log(clients);
 	})
 
+// when application disconnect with server
+	client.on('disconnect', (message) => {
+
+		console.log(`disconnected: ${message}`);
+
+	})
+
+// when an app wants to send message to another app
 	client.on('message', function(message) {
+
 		console.log(message);
-		notifiUser(message)
+		sendMessage(message);
+
+	})
+
+// bind username with socket id
+	client.on('verifyUser', function(message) {
+
+		console.log(`confirming user: ${message.userId} with socket: ${message.socketId}`);
+		users[message.userId] =  userSockets[message.socketId];
+		users[message.userId].emit("connectionConfirmedByServer", {});
+
+	}) 
+
+// when a socket disconnected
+	client.on('disconnectUser', function(message) {
+
+		console.log('disconnected ' + message.userId);
+
+	})
+
+// when a user reconnected
+	client.on('reconnectUser', function(message) {
+
+		console.log('reconnected ' + message.userId);
+
 	})
 });
 
-let notifiUser = (content) => {
+let sendMessage = (content) => {
 
-	clients[content.to].emit("newMessage", {message : content.message});
+	users[content.to].emit("newMessage", {message : content.message});
 
 }
 
 server.listen(3000);
+
+
+
+
+
+
+
+
+
+
